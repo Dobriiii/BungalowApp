170f5cf5338655ca951c4a3ff2aa97124c1619ec
dobri <dobri@>
2023-04-25 11:46:32 +0300

    Add project files.

---
+++ BungalowApp/Data/Bungalow.cs
@@ -0,0 +1,12 @@
+﻿namespace BungalowApp.Data
+{
+  public class Bungalow
+  {
+    public int Id { get; set; }   
+    public string BungalowId { get; set; }
+    public virtual AppUser? Assignee { get; set; }
+    public string ReservationListId { get; set; }
+    public virtual ReservationList? ReservationList { get;set; }
+
+  }
+}
